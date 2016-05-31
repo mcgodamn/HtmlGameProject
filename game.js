b3a99eb6a4e043c44672c965e3e2f4b7f41175ea
t7214948ttt <t7214948ttt@gmail.com>
2016-05-31 13:23:55 +0800

    左右完成 到達邊界後會不能動

--- game.js
+++ game.js
@@ -1,12 +1,12 @@
 ﻿function ini() {
-    xpos = -168;
+    xpos = -168; //紀錄目前位置
     active = 0; //可不可以按移動鍵 1 2 是不行 0 可以
     flagg = true; //是否在地上的旗幟
     document.getElementById("player1").style.position = "relative";
     document.getElementById("player1").style.top = "386px";
     document.getElementById("player1").style.left = xpos + "px";
     document.addEventListener('keypress', move, false);
-    document.addEventListener('keydown', move, false);
+    // document.addEventListener('keydown', move, false);
     // document.addEventListener('keyup', move, false);
 }
 
@@ -29,41 +29,43 @@ function move(event) {
 }
 
 function left() {
-    if ((active == 1) && (parseInt(document.getElementById("player1").style.left) > -373) && i < 5) {
-        i++;
+    if ((active == 1) && (xpos >= -373)) {
         xpos -= 1;
         document.getElementById("player1").style.left = xpos + "px";
         status = xpos;
-        setTimeout("left()", 2);
-        if(document.getElementById("player1").style.left == "-373px"){
-            active = 0;
+        if (i < 5) {
+            i++;
+            setTimeout("left()", 2);
+            if(document.getElementById("player1").style.left == "-373px"){
+                active = 0;
+            }
         }
+        else active = 0;
     }
-    else if ((active == 1) && (parseInt(document.getElementById("player1").style.left) > -373) && i == 5) {
-        xpos -= 1;
+    else if ((active == 1) && (xpos < -373)) {
+        xpos = -373;
         document.getElementById("player1").style.left = xpos + "px";
         status = xpos;
-        active = 0;
     }
-    else xpos += 1;
 }
 
 function right() {
-    if ((active == 2) && (parseInt(document.getElementById("player1").style.left) < 377)  && i < 5) {
-        i++;
+    if ((active == 2) && (xpos <= 377)) {
         xpos += 1;
         document.getElementById("player1").style.left = xpos + "px";
         status = xpos;
-        setTimeout("right()", 2);
-        if(document.getElementById("player1").style.left == "377px"){
-            active = 0;
+        if (i < 5) {
+            i++;
+            setTimeout("right()", 2);
+            if(document.getElementById("player1").style.left == "377px"){
+                active = 0;
+            }
         }
+        else active = 0;
     }
-    else if ((active == 2) && (parseInt(document.getElementById("player1").style.left) < 377)  && i == 5) {
-        xpos += 1;
+    else if ((active == 1) && (xpos > 373)) {
+        xpos = 373;
         document.getElementById("player1").style.left = xpos + "px";
         status = xpos;
-        active = 0;
     }
-    else xpos -= 1;
 }
