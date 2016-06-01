c70c803b57f3e83c277678d22042b6c9492df2b8
t7214948ttt <t7214948ttt@gmail.com>
2016-06-01 16:16:06 +0800

    新增跳躍 但跳躍會中斷走路動作

--- game.js
+++ game.js
@@ -1,31 +1,36 @@
 ﻿function ini() {
     xpos = -168; //紀錄目前位置
     active = 0; //可不可以按移動鍵 1 2 是不行 0 可以
-    flagg = true; //是否在地上的旗幟
+    skystatus = 'g'; // 'g' for ground , 'j' for jumping , 'a' for slashing to anemy , 'w' for hooking the wall
+    ypos = 386;
     document.getElementById("player1").style.position = "relative";
-    document.getElementById("player1").style.top = "386px";
+    document.getElementById("player1").style.top = ypos + "px";
     document.getElementById("player1").style.left = xpos + "px";
     document.addEventListener('keypress', move, false);
-    document.addEventListener('keydown', move, false);
-    // document.addEventListener('keyup', move, false);
+    document.addEventListener('keydown', move2, false);
 }
 
 function move(event) {
-    
     var myevent = event ? event : window.event;
-    if ((myevent.keyCode == 37) && !active) {
+    if (myevent.keyCode == 37) {
         active = 1;
         i = 0; //迴圈計數器
         left();
     }
-    else if ((myevent.keyCode == 39) && !active) {
+    else if (myevent.keyCode == 39) {
         active = 2;
         i = 0;
         right();
     }
-    // else if ((myevent.keyCode == 38) && !active) {
-    //     jump();
-    // }
+}
+
+function move2(event2) {
+    var myevent = event2 ? event2 : window.event;
+    if ((myevent.keyCode == 38) && (skystatus == 'g')) {
+        skystatus = 'j';
+        jstatus = 1; // 1 up , 0 down
+        jump(jstatus);
+    }
 }
 
 function left() {
@@ -33,9 +38,9 @@ function left() {
         xpos -= 1;
         document.getElementById("player1").style.left = xpos + "px";
         status = xpos;
-        if (i < 5) {
+        if (i < 40) {
             i++;
-            setTimeout("left()", 2);
+            setTimeout("left()", 1);
             if(document.getElementById("player1").style.left == "-373px"){
                 active = 0;
             }
@@ -55,9 +60,9 @@ function right() {
         xpos += 1;
         document.getElementById("player1").style.left = xpos + "px";
         status = xpos;
-        if (i < 5) {
+        if (i < 40) {
             i++;
-            setTimeout("right()", 2);
+            setTimeout("right()", 1);
             if(document.getElementById("player1").style.left == "377px"){
                 active = 0;
             }
@@ -72,6 +77,29 @@ function right() {
     }
 }
 
+function jump(jsta) {
+    if (skystatus == 'j') {
+        setTimeout(function(){
+            if (jsta == 1) {
+                ypos-=3;
+                document.getElementById("player1").style.top = ypos + "px";
+                if (ypos < 30) jstatus = 0;
+                jump(jstatus);
+            }
+            else if (jsta == 0) {
+                ypos+=3;
+                document.getElementById("player1").style.top = ypos + "px";
+                if (ypos >= 386) {
+                    skystatus = 'g';
+                    ypos = 386;
+                    document.getElementById("player1").style.top = ypos + "px";
+                }
+                jump(jstatus);
+            }
+        },1)
+    }
+}
+
 function test() {
-    document.write(xpos);
+    document.write(skystatus);
 }
