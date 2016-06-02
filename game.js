3fabeee6e9f999364e12c543bac98a496751dc9d
t7214948ttt <t7214948ttt@gmail.com>
2016-06-02 22:50:50 +0800

    Add files via upload

--- game.js
+++ game.js
@@ -1,78 +1,64 @@
 ﻿function ini() {
     xpos = -168; //紀錄目前位置
-    active = 0; //可不可以按移動鍵 1 2 是不行 0 可以
+    active = 0; //to record the move state , 1 for left , 2 for right , 0 for still
     skystatus = 'g'; // 'g' for ground , 'j' for jumping , 'a' for slashing to anemy , 'w' for hooking the wall
     ypos = 386;
     document.getElementById("player1").style.position = "relative";
     document.getElementById("player1").style.top = ypos + "px";
     document.getElementById("player1").style.left = xpos + "px";
-    document.addEventListener('keypress', move, false);
-    document.addEventListener('keydown', move2, false);
+    document.addEventListener('keydown', move, false);
+    document.addEventListener('keyup', move2, false);
 }
 
 function move(event) {
     var myevent = event ? event : window.event;
-    if (myevent.keyCode == 37) {
+    if ((myevent.keyCode == 38) && (skystatus == 'g')) {
+        skystatus = 'j';
+        jstatus = 1; // 1 up , 0 down
+        jump(jstatus);
+    }
+    if (myevent.keyCode == 37 && active != 1) {
         active = 1;
-        i = 0; //迴圈計數器
         left();
     }
-    else if (myevent.keyCode == 39) {
+    else if (myevent.keyCode == 39 && active != 2) {
         active = 2;
-        i = 0;
         right();
     }
 }
 
 function move2(event2) {
     var myevent = event2 ? event2 : window.event;
-    if ((myevent.keyCode == 38) && (skystatus == 'g')) {
-        skystatus = 'j';
-        jstatus = 1; // 1 up , 0 down
-        jump(jstatus);
+    if (myevent.keyCode == 37 && active == 1) {
+        active = 0;
+    }
+    else if (myevent.keyCode == 39 && active == 2) {
+        active = 0;
     }
 }
 
 function left() {
-    if ((active == 1) && (xpos >= -373)) {
+    if ((active == 1) && (xpos > -373)) {
         xpos -= 1;
         document.getElementById("player1").style.left = xpos + "px";
-        status = xpos;
-        if (i < 40) {
-            i++;
-            setTimeout("left()", 1);
-            if(document.getElementById("player1").style.left == "-373px"){
-                active = 0;
-            }
-        }
-        else active = 0;
+        setTimeout("left()", 1);
     }
-    else if ((active == 1) && (xpos < -373)) {
+    else if ((active == 1) && (xpos <= -373)) {
         xpos = -373;
         document.getElementById("player1").style.left = xpos + "px";
-        status = xpos;
         active = 0;
     }
 }
 
 function right() {
-    if ((active == 2) && (xpos <= 377)) {
+    if ((active == 2) && (xpos < 377)) {
         xpos += 1;
         document.getElementById("player1").style.left = xpos + "px";
-        status = xpos;
-        if (i < 40) {
-            i++;
-            setTimeout("right()", 1);
-            if(document.getElementById("player1").style.left == "377px"){
-                active = 0;
-            }
-        }
-        else active = 0;
+        setTimeout("right()", 1);;
     }
-    else if ((active == 2) && (xpos > 377)) {
+    else if ((active == 2) && (xpos >= 377)) {
         xpos = 377;
         document.getElementById("player1").style.left = xpos + "px";
-        status = xpos;
         active = 0;
     }
 }
@@ -83,7 +69,7 @@ function jump(jsta) {
             if (jsta == 1) {
                 ypos-=3;
                 document.getElementById("player1").style.top = ypos + "px";
-                if (ypos < 30) jstatus = 0;
+                if (ypos < 200) jstatus = 0;
                 jump(jstatus);
             }
             else if (jsta == 0) {
