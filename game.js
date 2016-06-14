51c527ff107a3b4f9a22e37094a9bc832f9e6abe
鍾孟儒 <t7214948ttt@gmail.com>
2016-06-14 18:12:12 +0800

    Add files via upload

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-﻿function ini() {
+function ini() {
     xpos = -168; //紀錄目前位置
     active = 0; //to record the move state , 1 for left , 2 for right , 0 for still
     // 'g' for ground , 'j' for jumping , 's' for slashing to anemy , 'c' for get close animation after slash, 'w' for hooking the wall
@@ -10,6 +10,7 @@
     c = 0;
     upf = 0; //up arrow flag
     dof = 0; //down arrow flag
+    wv = 0;
     document.getElementById("player1").style.position = "relative";
     document.getElementById("player1").style.top = ypos + "px";
     document.getElementById("player1").style.left = xpos + "px";
@@ -38,7 +39,7 @@ function move(event) {
     }
     else if (myevent.keyCode == 88 && skystatus != 'w') { //x
         cxt.clearRect(0, 0, canvas.width, canvas.height);
-        if (upf == 1 && active == 0) { //上
+        if (upf == 1 && wv == 0) { //上
             cxt.beginPath();
             cxt.moveTo(xpos + 400,ypos + 40);
             cxt.lineTo(xpos + 400, 0);
@@ -46,9 +47,9 @@ function move(event) {
             cxt.stroke();
             skystatus = 'w';
             swf = 1;
-            wall(upf,dof,active);
+            wall(upf,dof,wv);
         }
-        else if (dof == 1 && active == 0 && skystatus != 'g') { //下
+        else if (dof == 1 && wv == 0 && skystatus != 'g') { //下
             cxt.beginPath();
             cxt.moveTo(xpos + 400,ypos + 40);
             cxt.lineTo(xpos + 400, 480);
@@ -56,9 +57,9 @@ function move(event) {
             cxt.stroke();
             skystatus = 'w';
             swf = 1;
-            wall(upf,dof,active);
+            wall(upf,dof,wv);
         }
-        else if (upf == 0 && dof ==0 && active == 1) { //右
+        else if (upf == 0 && dof ==0 && wv == 1) { //右
             cxt.beginPath();
             cxt.moveTo(xpos + 400,ypos + 40);
             cxt.lineTo(0, ypos + 40);
@@ -66,9 +67,9 @@ function move(event) {
             cxt.stroke();
             skystatus = 'w';
             swf = 1;
-            wall(upf,dof,active);
+            wall(upf,dof,wv);
         }
-        else if (upf == 0 && dof ==0 && active == 2) { //左
+        else if (upf == 0 && dof ==0 && wv == 2) { //左
             cxt.beginPath();
             cxt.moveTo(xpos + 400,ypos + 40);
             cxt.lineTo(800, ypos + 40);
@@ -76,9 +77,9 @@ function move(event) {
             cxt.stroke();
             skystatus = 'w';
             swf = 1;
-            wall(upf,dof,active);
+            wall(upf,dof,wv);
         }
-        else if (upf == 1 && active == 2) { //左上
+        else if (upf == 1 && wv == 2) { //左上
             cxt.beginPath();
             cxt.moveTo(xpos + 400,ypos + 40);
             cxt.lineTo(800, ypos + 40 - (400-xpos));
@@ -86,9 +87,9 @@ function move(event) {
             cxt.stroke();
             skystatus = 'w';
             swf = 1;
-            wall(upf,dof,active);
+            wall(upf,dof,wv);
         }
-        else if (upf == 1 && active == 1) { //右上
+        else if (upf == 1 && wv == 1) { //右上
             cxt.beginPath();
             cxt.moveTo(xpos + 400,ypos + 40);
             cxt.lineTo(0, ypos + 40 - (400+xpos));
@@ -96,9 +97,9 @@ function move(event) {
             cxt.stroke();
             skystatus = 'w';
             swf = 1;
-            wall(upf,dof,active);
+            wall(upf,dof,wv);
         }
-        else if (dof == 1 && active == 1 && skystatus != 'g') { //左下
+        else if (dof == 1 && wv == 1 && skystatus != 'g') { //左下
             cxt.beginPath();
             cxt.moveTo(xpos + 400,ypos + 40);
             cxt.lineTo(xpos + ypos - 40, 480);
@@ -106,9 +107,9 @@ function move(event) {
             cxt.stroke();
             skystatus = 'w';
             swf = 1;
-            wall(upf,dof,active);
+            wall(upf,dof,wv);
         }
-        else if (dof == 1 && active == 2 && skystatus != 'g') { //右下
+        else if (dof == 1 && wv == 2 && skystatus != 'g') { //右下
             cxt.beginPath();
             cxt.moveTo(xpos + 400,ypos + 40);
             cxt.lineTo(xpos + 840 - ypos, 480);
@@ -116,7 +117,7 @@ function move(event) {
             cxt.stroke();
             skystatus = 'w';
             swf = 1;
-            wall(upf,dof,active);
+            wall(upf,dof,wv);
         }
     }
     if ((myevent.keyCode == 32) && (skystatus == 'g')) { //space
@@ -125,17 +126,25 @@ function move(event) {
         jump(jstatus);
     }
     if ((myevent.keyCode == 37) && (active != 1)) {
-        active = 1;
         if (swf == 0) {
+            active = 1;
             document.getElementById("test").src="playerL.png";
             left();
         }
+        else {
+            active = 0;
+            wv = 1;
+        }
     }
     else if ((myevent.keyCode == 39) && (active != 2)) {
-        active = 2;
         if (swf == 0) {
+            active = 2;
             document.getElementById("test").src="playerR.png";
-            right();   
+            right();
+        }
+        else {
+            active = 0;
+            wv = 2;
         }
     }
     if (myevent.keyCode == 38) {
@@ -151,9 +160,14 @@ function move(event) {
 function move2(event2) {
     var myevent = event2 ? event2 : window.event;
 
-    if (myevent.keyCode == 37 && active == 1) active = 0;
-    else if (myevent.keyCode == 39 && active == 2) active = 0;
-
+    if (myevent.keyCode == 37 && active == 1) {
+        active = 0;
+        wv = 0;
+    }
+    else if (myevent.keyCode == 39 && active == 2) {
+        active = 0;
+        wv = 0;
+    }
     if (myevent.keyCode == 38 && upf == 1) upf = 0;
     else if (myevent.keyCode == 38 && dof == 1) dof = 0;
 }
@@ -289,16 +303,27 @@ function wall(u,d,v) {
                     wall(0,1,0);
                 }
                 else if (hori == 0 && verti == 1) { //左
-                    cleary = ypos+30;
-                    clearx = xpos + 380;
                     xpos-=3;
+                    cleary = ypos+30;
+                    clearx = xpos + 400;
                     document.getElementById("player1").style.top = ypos + "px";
-                    cxt.clearRect(clearx, cleary, 30, 3);
+                    cxt.clearRect(clearx, cleary, 25, 30);
                     wall(0,0,1);
                 }
-
+                else if (hori == 0 && verti == 2) { //right
+                    xpos+=3;
+                    cleary = ypos+30;
+                    clearx = xpos + 400;
+                    document.getElementById("player1").style.top = ypos + "px";
+                    cxt.clearRect(clearx, cleary, 25, 30);
+                    wall(0,0,2);
+                }
             }
-            else if ((hori == 1 && verti == 0) ||(hori == 0 && verti == 1)) cxt.clearRect(0, 0, canvas.width, canvas.height);
+            else if (hori == 1 && verti == 0) {
+                swf = 0;
+                skystatus = 'g';
+                cxt.clearRect(0, 0, canvas.width, canvas.height);
+            }
         },1)
     }
 }
