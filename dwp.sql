e12790846c092be053b8f414bdf0f8eca3f9c900
will10227 <will10227@gmail.com>
2016-06-20 08:32:28 +0800

    Add files via upload

--- dwp.sql
+++ dwp.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主機: 127.0.0.1
--- 產生時間： 2016-06-19 16:23:22
+-- 產生時間： 2016-06-20 02:31:37
 -- 伺服器版本: 10.1.13-MariaDB
 -- PHP 版本： 5.6.21
 
@@ -30,6 +30,8 @@ CREATE TABLE `member` (
   `ID` char(20) NOT NULL,
   `password` char(20) NOT NULL,
   `skill` int(1) NOT NULL DEFAULT '1',
+  `skill2` int(1) NOT NULL DEFAULT '0',
+  `skill3` int(1) DEFAULT '0',
   `image` char(150) NOT NULL,
   `usingSkill` int(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
